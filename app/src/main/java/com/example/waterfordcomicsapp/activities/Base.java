32dd744e289c71a290176c53cc7db0d7ebb8008c
CRea7 <conorrea123@gmail.com>
2019-02-05 16:05:43 +0000

    api data displaying as ToString to screen need to filter

--- app/src/main/java/com/example/waterfordcomicsapp/activities/Base.java
+++ app/src/main/java/com/example/waterfordcomicsapp/activities/Base.java
@@ -1,8 +1,100 @@
 package com.example.waterfordcomicsapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.waterfordcomicsapp.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Base extends AppCompatActivity {
+    String ReturnString;
+    //Declare a private RequestQueue variable
 
+    private static Base mInstance;
+
+
+
+
+
+    public void test(RequestQueue requestQueue){
+
+        String url = "http://gateway.marvel.com/v1/public/comics?ts=" + getString(R.string.ts) +"&apikey="+ getString(R.string.public_key) +"&hash=" + getString(R.string.hash);
+
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try{
+                            JSONObject init_list = response.getJSONObject("data");
+                            //JSONArray result_list = init_list.getJSONArray("results");
+                            Log.i("INFO",init_list.toString());
+                            ReturnString = init_list.toString();
+                        }
+                        catch(Exception e)
+                        {
+                            Log.i("ERROR",e.toString());
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("TEST",error.toString());
+                    }
+                });
+        // Adding the request to the queue along with a unique string tag
+        requestQueue.add(jsonObjReq);
+
+    }
+
+
+
+
+
+
+//    public static synchronized Base getInstance() {
+//        return mInstance;
+//    }
+//    public RequestQueue getRequestQueue() {
+//        if (requestQueue == null)
+//            requestQueue = Volley.newRequestQueue(this);
+//        return requestQueue;
+//    }
+//    public void addToRequestQueue(Request request, String tag) {
+//        request.setTag(tag);
+//        getRequestQueue().add(request);
+//    }
+//    public void cancelAllRequests(String tag) {
+//        getRequestQueue().cancelAll(tag);
+//    }
+
+//    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+//            url, (String) null,
+//            new Response.Listener<JSONObject>() {
+//                @Override
+//                public void onResponse(JSONObject response) {
+//                    //Success Callback
+//                }
+//            },
+//            new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    //Failure Callback
+//                }
+//            });
+//    // Adding the request to the queue along with a unique string tag
+//    requestQueue.add(jsonObjectReq);
 
 }
--- app/src/main/java/com/example/waterfordcomicsapp/activities/MainActivity.java
+++ app/src/main/java/com/example/waterfordcomicsapp/activities/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.waterfordcomicsapp.activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.example.waterfordcomicsapp.R;
 
-public class MainActivity extends Base {
+public class MainActivity extends Base{
+    RequestQueue requestQueue;
+    Button getButton;
+    TextView responseTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +26,23 @@ public class MainActivity extends Base {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        requestQueue = Volley.newRequestQueue(this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                test(requestQueue);
+                String Text_View = ReturnString;
+                responseTv =  findViewById(R.id.response);
+                responseTv.setText(Text_View);
             }
         });
+
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
