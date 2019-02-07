c37bd41fdefa6d9de971e98203386880f43a0881
CRea7 <conorrea123@gmail.com>
2019-02-07 14:31:16 +0000

    data reading to comic list need to display nice and add image check

--- app/src/main/java/com/example/waterfordcomicsapp/models/Comic.java
+++ app/src/main/java/com/example/waterfordcomicsapp/models/Comic.java
@@ -1,31 +1,34 @@
-package com.example.models;
+package com.example.waterfordcomicsapp.models;
 
 import java.io.Serializable;
 
 public class Comic implements Serializable
 {
     public String comicId;
-    public String comicName;
+    public String comicTitle;
     public String image;
+    public String extension;
     public String issueNumber;
     public String storeDate;
-    public String description;
+    public String price;
 
 
     public Comic() {}
 
-    public Comic(String name, String image, String issueNumber, String storeDate, String description)
+    public Comic(String comicId, String Title, String image,String extension, String issueNumber, String storeDate, String price)
     {
-        this.comicName = name;
+        this.comicId = comicId;
+        this.comicTitle = Title;
         this.image = image;
+        this.extension = extension;
         this.issueNumber = issueNumber;
         this.storeDate = storeDate;
-        this.description = description;
+        this.price = price;
     }
 
     @Override
     public String toString() {
-        return comicName + ", " + image + ", " + issueNumber
-                + ", " + storeDate + ", " + description;
+        return comicId + ", " + comicTitle + ", " + image + ", " + extension + ", " + issueNumber
+                + ", " + storeDate + ", " + price;
     }
 }
