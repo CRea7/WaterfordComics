c6a75749ce37136ea87566d894c00f3c8ab0a0c6
CRea7 <conorrea123@gmail.com>
2019-04-29 18:03:07 +0100

    changes

---
+++ app/src/main/java/com/example/waterfordcomicsapp/activities/Help.java
@@ -0,0 +1,34 @@
+package com.example.waterfordcomicsapp.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+
+import com.example.waterfordcomicsapp.R;
+
+public class Help extends NavDrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        @SuppressLint("InflateParams")
+        View contentView = inflater.inflate(R.layout.activity_sign_up_in, null, false);
+        drawer.addView(contentView,0);
+        getSupportActionBar().setTitle("Help");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        return true;
+    }
+}
